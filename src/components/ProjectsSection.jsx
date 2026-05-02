2c0d98e65fd579c694d876971a4b2511fcb92052
Maaaccc05 <mackam30@gmail.com>
2026-05-02 23:37:26 +0530

    justtt

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -42,6 +42,7 @@ export const ProjectsSection = () => {
     >
       <div className="container mx-auto max-w-5xl">
         {/* Header */}
+        
         <div
           style={{
             display: "flex",
